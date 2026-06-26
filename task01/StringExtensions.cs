f8e5472f3eded779beaed16709bd1b9394b98ce8
Ktololo <lyuda.litvinova2007@gmail.com>
2026-06-26 19:43:00 +0600

    Реализован метод IsPalindrome и написаны тесты

---
+++ task01/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace task01
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+            string lower = input.ToLower();
+            string cleaned = new string(lower.Where(c => !char.IsPunctuation(c) && !char.IsWhiteSpace(c)).ToArray());
+            string reversed = new string(cleaned.Reverse().ToArray());
+            return cleaned == reversed;
+        }
+    }
+}
