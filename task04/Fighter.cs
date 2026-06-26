b1b3e4fbe1b537b42938dabc76519593184fd16b
Ktololo <lyuda.litvinova2007@gmail.com>
2026-06-27 03:03:01 +0600

    Реализованы интерфейсы ISpaceship, Cruiser и Fighter, тесты

---
+++ task04/Fighter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task04
+{
+    public class Fighter : ISpaceship
+    {
+        private int _angle;
+        public int Speed => 100;
+        public int FirePower => 50;
+
+        public void MoveForward()
+        {
+            Console.WriteLine($"Истребитель движется вперед со скоростью {Speed}");
+        }
+        public void Rotate(int angle)
+        {
+            _angle = (_angle + angle) % 360;
+            Console.WriteLine($"Истребитель повернут на угол {_angle} гр");
+        }
+        public void Fire()
+        {
+            Console.WriteLine($"Истребитель стреляет легкими ракетами, Мощность: {FirePower}");
+        }
+    }
+}
