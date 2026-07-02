8371b67ee6c9981b7f310857e1f4438dbcdbdfe1
Ktololo <lyuda.litvinova2007@gmail.com>
2026-07-02 19:39:27 +0300

    Реализована динамическая загрузка библиотек

---
+++ FileSystemCommands/FindFilesCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+public class FindFilesCommand : ICommand
+{
+    private readonly string _directoryPath;
+    private readonly string _mask;
+    public FindFilesCommand(string directoryPath, string mask)
+    {
+        _directoryPath = directoryPath;
+        _mask = mask;
+    }
+
+    public void Execute()
+    {
+        if (!Directory.Exists(_directoryPath))
+        {
+            Console.WriteLine($"Каталог {_directoryPath} не существует.");
+            return;
+        }
+        var files = Directory.GetFiles(_directoryPath, _mask, SearchOption.AllDirectories);
+        Console.WriteLine($"Найдено файлов по маске '{_mask}': {files.Length}");
+        foreach (var file in files)
+        {
+            Console.WriteLine($"  {file}");
+        }
+    }
+}
