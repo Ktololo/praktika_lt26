c3f41c4b43540c7bdb30cc93215a0b83a33be108
Ktololo <164876316+Ktololo@users.noreply.github.com>
2026-07-14 12:48:27 +0300

    Update Cruiser.cs

--- task04/Cruiser.cs
+++ task04/Cruiser.cs
@@ -1,24 +1,25 @@
-﻿using System;
-
 namespace task04
 {
     public class Cruiser : ISpaceship
     {
         private int _angle;
+
         public int Speed => 50;
         public int FirePower => 100;
+
         public void MoveForward()
         {
-            Console.WriteLine($"Крейсер движется вперед со скоростью {Speed}");
+
         }
+
         public void Rotate(int angle)
         {
             _angle = (_angle + angle) % 360;
-            Console.WriteLine($"Крейсер повернут на угол {_angle} градусов");
         }
+
         public void Fire()
         {
-            Console.WriteLine($"Крейсер стреляет мощными ракетами! Мощность: {FirePower}");
+
         }
     }
 }
