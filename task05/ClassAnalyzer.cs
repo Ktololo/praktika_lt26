77892d1cd27141e842fbd3a8b21d64bf4425f05e
Ktololo <164876316+Ktololo@users.noreply.github.com>
2026-07-14 12:55:55 +0300

    Update ClassAnalyzer.cs

--- task05/ClassAnalyzer.cs
+++ task05/ClassAnalyzer.cs
@@ -1,41 +1,42 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
 public class ClassAnalyzer
 {
-    private Type _type;
+    private readonly Type _type;
+
     public ClassAnalyzer(Type type)
     {
+        if (type == null)
+            throw new ArgumentNullException(nameof(type));
         _type = type;
     }
     public IEnumerable<string> GetPublicMethods()
     {
         return _type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)
-            .Select(m => m.Name);
+                    .Select(m => m.Name);
     }
     public IEnumerable<string> GetMethodParams(string methodName)
     {
         var method = _type.GetMethod(methodName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
-
         if (method == null)
-            return new List<string>();
-
+            return Enumerable.Empty<string>();
         return method.GetParameters().Select(p => $"{p.ParameterType.Name} {p.Name}");
     }
     public IEnumerable<string> GetAllFields()
     {
         return _type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly)
-            .Select(f => f.Name);
+                    .Select(f => f.Name);
     }
     public IEnumerable<string> GetProperties()
     {
         return _type.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)
-            .Select(p => p.Name);
+                    .Select(p => p.Name);
     }
     public bool HasAttribute<T>() where T : Attribute
     {
-        return _type.GetCustomAttribute<T>() != null;
+        return _type.IsDefined(typeof(T), inherit: false);
     }
 }
