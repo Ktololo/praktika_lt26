78e825638441df64053c01732bdb46175bae7b43
Ktololo <164876316+Ktololo@users.noreply.github.com>
2026-07-15 22:17:39 +0300

    Update PluginTests.cs

--- task10tests/PluginTests.cs
+++ task10tests/PluginTests.cs
@@ -1,7 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
 using System.Linq;
+using System.Reflection;
 using Xunit;
 
 public class PluginTests
@@ -23,7 +24,6 @@ public class PluginTests
         var type = typeof(PluginB);
         var instance = Activator.CreateInstance(type);
         var command = instance as ICommand;
-        
         Assert.NotNull(command);
         var ex = Record.Exception(() => command.Execute());
         Assert.Null(ex);
@@ -34,11 +34,11 @@ public class PluginTests
         var type = typeof(PluginC);
         var instance = Activator.CreateInstance(type);
         var command = instance as ICommand;
-        
         Assert.NotNull(command);
         var ex = Record.Exception(() => command.Execute());
         Assert.Null(ex);
     }
+
     [Fact]
     public void PluginC_DependsOnPluginA()
     {
@@ -46,6 +46,7 @@ public class PluginTests
         Assert.NotNull(attr);
         Assert.Equal("PluginA", attr.DependsOn);
     }
+
     [Fact]
     public void PluginLoader_ShouldLoadPluginsWithoutError()
     {
@@ -61,6 +62,39 @@ public class PluginTests
                 .ToList();
             pluginTypes.AddRange(types);
         }
+
         Assert.NotEmpty(pluginTypes);
+    }
+
+    [Fact]
+    public void PluginLoader_TopologicalSort_ShouldOrderCorrectly()
+    {
+        var types = new List<Type> { typeof(PluginC), typeof(PluginA), typeof(PluginB) };
+        var sorted = TopologicalSort(types);
+        Assert.Equal(3, sorted.Count);
+        Assert.True(sorted.IndexOf(typeof(PluginA)) < sorted.IndexOf(typeof(PluginC)));
+    }
+
+    private List<Type> TopologicalSort(List<Type> types)
+    {
+        var result = new List<Type>();
+        var visited = new HashSet<Type>();
+        foreach (var type in types)
+            Visit(type, types, visited, result);
+        return result;
+    }
+
+    private void Visit(Type type, List<Type> all, HashSet<Type> visited, List<Type> result)
+    {
+        if (visited.Contains(type)) return;
+        visited.Add(type);
+        var attr = type.GetCustomAttribute<PluginLoadAttribute>();
+        if (!string.IsNullOrEmpty(attr?.DependsOn))
+        {
+            var dep = all.FirstOrDefault(t => t.Name == attr.DependsOn);
+            if (dep != null)
+                Visit(dep, all, visited, result);
+        }
+        result.Add(type);
     }
 }
