bf97c8c8e34c5af84531c7d6243ac1eaada44303
Ktololo <lyuda.litvinova2007@gmail.com>
2026-07-15 23:29:15 +0300

    Реализация потока обработки команад

---
+++ task17/ServerThread.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+public class ServerThread
+{
+    private readonly BlockingCollection<ICommand> _queue = new BlockingCollection<ICommand>();
+    private readonly Thread _thread;
+    private bool _softStopRequested = false;
+    private bool _hardStopRequested = false;
+    public ServerThread()
+    {
+        _thread = new Thread(Run);
+        _thread.Start();
+    }
+    public void AddCommand(ICommand command)
+    {
+        if (command == null)
+            throw new ArgumentNullException(nameof(command));
+        if (_hardStopRequested)
+            throw new InvalidOperationException("Поток остановлен HardStop");
+
+        _queue.Add(command);
+    }
+    public void StopSoft()
+    {
+        AddCommand(new SoftStopCommand(this));
+    }
+    public void StopHard()
+    {
+        AddCommand(new HardStopCommand(this));
+    }
+    public bool IsAlive => _thread.IsAlive;
+    private void Run()
+    {
+        try
+        {
+            while (!_hardStopRequested)
+            {
+                if (_softStopRequested && _queue.Count == 0)
+                    break;
+
+                if (_queue.TryTake(out var command, 100))
+                {
+                    try
+                    {
+                        command.Execute();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Ошибка выполнения: {ex.Message}");
+                    }
+                }
+            }
+        }
+        catch (ThreadInterruptedException)
+        {
+
+        }
+        finally
+        {
+            _queue.Dispose();
+        }
+    }
+    private class SoftStopCommand : ICommand
+    {
+        private readonly ServerThread _server;
+        public SoftStopCommand(ServerThread server)
+        {
+            _server = server;
+        }
+        public void Execute()
+        {
+            if (Thread.CurrentThread != _server._thread)
+                throw new InvalidOperationException("SoftStop должен выполняться в том же потоке");
+
+            _server._softStopRequested = true;
+        }
+    }
+    private class HardStopCommand : ICommand
+    {
+        private readonly ServerThread _server;
+        public HardStopCommand(ServerThread server)
+        {
+            _server = server;
+        }
+        public void Execute()
+        {
+            if (Thread.CurrentThread != _server._thread)
+                throw new InvalidOperationException("HardStop должен выполняться в том же потоке");
+            _server._hardStopRequested = true;
+            _server._thread.Interrupt();
+        }
+    }
+}
---
+++ task17tests/ServerThreadTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading;
+using Xunit;
+
+public class ServerThreadTests
+{
+    [Fact]
+    public void AddCommand_ExecutesCommand()
+    {
+        var server = new ServerThread();
+        bool executed = false;
+
+        server.AddCommand(new TestCommand(() => executed = true));
+        Thread.Sleep(100);
+
+        Assert.True(executed);
+    }
+
+    [Fact]
+    public void SoftStop_StopsAfterQueueEmpty()
+    {
+        var server = new ServerThread();
+        bool executed = false;
+
+        server.AddCommand(new TestCommand(() => executed = true));
+        server.StopSoft();
+
+        Thread.Sleep(200);
+        Assert.False(server.IsAlive);
+        Assert.True(executed);
+    }
+
+    [Fact]
+    public void HardStop_StopsImmediately()
+    {
+        var server = new ServerThread();
+        bool executed = false;
+
+        server.AddCommand(new TestCommand(() =>
+        {
+            Thread.Sleep(500);
+            executed = true;
+        }));
+
+        Thread.Sleep(50);
+        server.StopHard();
+
+        Thread.Sleep(100);
+        Assert.False(server.IsAlive);
+        Assert.False(executed);
+    }
+
+    [Fact]
+    public void StopCommand_FromWrongThread_ThrowsException()
+    {
+        var server = new ServerThread();
+        var exception = Record.Exception(() =>
+        {
+            var command = new TestCommand(() => { });
+            server.AddCommand(command);
+        });
+
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void AddCommand_AfterHardStop_ThrowsException()
+    {
+        var server = new ServerThread();
+        server.StopHard();
+        Thread.Sleep(50);
+
+        Assert.Throws<InvalidOperationException>(() => server.AddCommand(new TestCommand(() => { })));
+    }
+
+    private class TestCommand : ICommand
+    {
+        private readonly Action _action;
+
+        public TestCommand(Action action)
+        {
+            _action = action;
+        }
+        public void Execute()
+        {
+            _action();
+        }
+    }
+}
