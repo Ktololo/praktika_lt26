1a0dce888e031c2affa7c56a7a0692f7df0ea9a4
Ktololo <164876316+Ktololo@users.noreply.github.com>
2026-07-15 22:35:09 +0300

    Update CalculatorWrapper.cs

--- task11/CalculatorWrapper.cs
+++ task11/CalculatorWrapper.cs
@@ -1,24 +1,13 @@
-﻿using System;
-using System.Reflection;
-
 public class CalculatorWrapper
 {
-    private readonly object _calc;
-    private readonly MethodInfo _add;
-    private readonly MethodInfo _minus;
-    private readonly MethodInfo _mul;
-    private readonly MethodInfo _div;
+    private readonly ICalculator _calc;
+
     public CalculatorWrapper()
     {
         _calc = CalculatorGenerator.CreateCalculator();
-        var t = _calc.GetType();
-        _add = t.GetMethod("Add");
-        _minus = t.GetMethod("Minus");
-        _mul = t.GetMethod("Mul");
-        _div = t.GetMethod("Div");
     }
-    public int Add(int a, int b) => (int)_add.Invoke(_calc, new object[] { a, b });
-    public int Minus(int a, int b) => (int)_minus.Invoke(_calc, new object[] { a, b });
-    public int Mul(int a, int b) => (int)_mul.Invoke(_calc, new object[] { a, b });
-    public int Div(int a, int b) => (int)_div.Invoke(_calc, new object[] { a, b });
+    public int Add(int a, int b) => _calc.Add(a, b);
+    public int Minus(int a, int b) => _calc.Minus(a, b);
+    public int Mul(int a, int b) => _calc.Mul(a, b);
+    public int Div(int a, int b) => _calc.Div(a, b);
 }
